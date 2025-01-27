68f87767ad5d504d16264ef394ee2b04a0e73b2f
Jasper McLean <jaspermclean10@gmail.com>
2025-01-26 19:19:43 -0600

    refactor + commenting

--- Assets/DroneController.cs
+++ Assets/DroneController.cs
@@ -3,64 +3,133 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Maneuvers the drone to the target position
+/// </summary>
 public class DroneController : MonoBehaviour
 {
+    /// <summary>
+    /// References to the four propellers in order of top left, top right, bottom left, bottom right
+    /// </summary>
     [SerializeField] private Propeller[] props;
 
+    /// <summary>
+    /// PID controller coefficient
+    /// </summary>
     [SerializeField] private Vector3 movementSpeed, movementDampening, rotationSpeed, rotationDampening;
 
-    [SerializeField] private Vector3 targetPos;
-    [SerializeField] private Transform targetTransform;
-    private Vector3 targetForce = Vector3.zero;
-
-    private Rigidbody _rigid;
-
-    private Vector3 _lastPosError = Vector3.zero;
-    private Vector3 _lastDirError = Vector3.zero;
-    private Vector3 PosError
+    /// <summary>
+    /// Position the drone will navigate towards.
+    /// </summary>
+    public Vector3 TargetPos
     {
         get
         {
-            return targetPos - transform.position;
+            if (targetTransform) return targetTransform.position;
+            return _targetPos;
         }
+        set
+        {
+            if (!targetTransform) _targetPos = value;
+        }
+        
     }
     
+    private Vector3 _targetPos;
+    
+    /// <summary>
+    /// If set, <see cref="TargetPos"/> will be locked to the position of <see cref="targetTransform"/>.
+    /// </summary>
+    public Transform targetTransform;
+    
+    private Vector3 _lastTargetPos = Vector3.zero;
+    private Vector3 _lastPosError = Vector3.zero;
+    private Vector3 _lastRotError = Vector3.zero;
+    
+    private Rigidbody _rigid;
+
+    /// <summary>
+    /// Target force to apply for the drone to navigate to <see cref="TargetPos"/>. Controlled by positional PID
+    /// </summary>
+    private Vector3 TargetForce()
+    {
+        Vector3 proportionalInput = PosError;
+        proportionalInput.x *= movementSpeed.x;
+        proportionalInput.y *= movementSpeed.y;
+        proportionalInput.z *= movementSpeed.z;
+        proportionalInput = Vector3.ClampMagnitude(proportionalInput, TotalMaxForce);
+
+        Vector3 dampeningInput = PosErrorDelta;
+        dampeningInput.x *= movementDampening.x;
+        dampeningInput.y *= movementDampening.y;
+        dampeningInput.z *= movementDampening.z;
+
+        Vector3 externalForceInput = -Physics.gravity * _rigid.mass;
+        
+        Vector3 targetForce = proportionalInput + dampeningInput + externalForceInput;
+        Debug.DrawRay(transform.position, targetForce, Color.red);
+
+        return targetForce;
+    }
+    
+    /// <summary>
+    /// Difference between drone position and target position
+    /// </summary>
+    private Vector3 PosError => TargetPos - transform.position;
+
+    /// <summary>
+    /// Change in positional error this physics step
+    /// </summary>
     private Vector3 PosErrorDelta
     {
         get
         {
-            return PosError - _lastPosError;
+            // Change in error should not include movement of the target position
+            // Only account for changes in error due to the drone's movement
+            Vector3 targetPosDelta = _lastTargetPos - TargetPos;
+            return PosError - _lastPosError + targetPosDelta;
         }
     }
     
-    private Vector3 DirError
+    /// <summary>
+    /// Rotational error
+    /// </summary>
+    private Vector3 RotError(Vector3 targetForce)
     {
-        get
+        // Rotation error is X and Z axis of target force in local space
+        // Drone's Y axis should align with target force vector
+        Vector3 dirError = transform.InverseTransformVector(targetForce);
+        
+        // Do not flip drone when descending, reverse propeller
+        if (targetForce.y < 0)
         {
-            Vector3 dirError = transform.InverseTransformVector(targetForce);
-            if (targetForce.y < 0)
-            {
-                dirError = -dirError;
-            }
-            
-            float yawInputDefault = Vector3.SignedAngle(transform.forward, Vector3.forward, transform.up) / 360f;
-            float yawInputTowardsTarget = Vector3.SignedAngle(transform.forward, PosError, transform.up) / 360f;
-            Vector3 longitudinalError = Vector3.ProjectOnPlane(PosError, Vector3.up);
-            dirError.y = Mathf.Lerp(yawInputDefault, yawInputTowardsTarget, longitudinalError.magnitude - 3f);
-            
-            return dirError;
-            
+            dirError = -dirError;
         }
+        
+        // Yaw to face the target position. If arrived at the target position, yaw to face world forward.
+        float yawInputTowardsTarget = Vector3.SignedAngle(transform.forward, PosError, transform.up) / 360f;
+        float yawInputDefault = Vector3.SignedAngle(transform.forward, Vector3.forward, transform.up) / 360f;
+        // Only yaw to face target position when not arrived at target position
+        // Do not yaw if 
+        Vector3 longitudinalError = Vector3.ProjectOnPlane(PosError, Vector3.up);
+        dirError.y = Mathf.Lerp(yawInputDefault, yawInputTowardsTarget, longitudinalError.magnitude - 3f);
+        
+        return dirError;
     }
     
-    private Vector3 DirErrorDelta
+    /// <summary>
+    /// Change in rotation error this physics step
+    /// </summary>
+    /// <param name="rotError"></param>
+    /// <returns></returns>
+    private Vector3 RotErrorDelta(Vector3 rotError)
     {
-        get
-        {
-            return DirError - _lastDirError;
-        }
+        return rotError - _lastRotError;
     }
 
+    /// <summary>
+    /// The maximum total force that can be applied by all four propellers
+    /// </summary>
     private float TotalMaxForce
     {
         get
@@ -76,63 +145,52 @@ public class DroneController : MonoBehaviour
         }
     }
 
+    private Vector3 RotationInput(Vector3 rotError, Vector3 rotErrorDelta)
+    {
+        Vector3 rotationProportionalInput = new Vector3(
+            rotError.x * rotationSpeed.x, 
+            rotError.y * rotationSpeed.y,
+            rotError.z * rotationSpeed.z);
+        
+        Vector3 rotationDampeningInput = new Vector3(
+            rotErrorDelta.x * rotationDampening.x, 
+            rotErrorDelta.y * rotationDampening.y, 
+            rotErrorDelta.z * rotationDampening.z);
+        
+        Vector3 rotationInput = rotationProportionalInput + rotationDampeningInput;
+        return rotationInput;
+    }
+
     private void Awake()
     {
         _rigid = GetComponent<Rigidbody>();
         _lastPosError = PosError;
-        _lastDirError = DirError;
+        _lastRotError = RotError(TargetForce());
     }
 
     private void FixedUpdate()
     {
-        if (targetTransform) targetPos = targetTransform.position;
+        Vector3 targetForce = TargetForce();
         
-        Vector3 proportionalInput = PosError;
-        proportionalInput.x *= movementSpeed.x;
-        proportionalInput.y *= movementSpeed.y;
-        proportionalInput.z *= movementSpeed.z;
-        proportionalInput = Vector3.ClampMagnitude(proportionalInput, TotalMaxForce);
-
-        Vector3 dampeningInput = PosErrorDelta;
-        dampeningInput.x *= movementDampening.x;
-        dampeningInput.y *= movementDampening.y;
-        dampeningInput.z *= movementDampening.z;
-        //dampeningInput = Vector3.ClampMagnitude(dampeningInput, TotalMaxForce);
-
-        Vector3 externalForceInput = -Physics.gravity * _rigid.mass;
-
-        targetForce = proportionalInput + dampeningInput + externalForceInput;
-        //targetForce = Vector3.ClampMagnitude(targetForce, TotalMaxForce);
-        Debug.DrawRay(transform.position, targetForce, Color.red);
-        
-        // Vertical Axis
+        // Set base throttle for all props to keep drone hovering / moving vertically towards target
         foreach (Propeller prop in props)
         {
             prop.Throttle = Vector3.Dot(transform.up, targetForce);
         }
-
-        //targetForce.y = 0;
         
-        Vector3 rotationProportionalInput = new Vector3(
-            DirError.x * rotationSpeed.x, 
-            DirError.y * rotationSpeed.y,
-            DirError.z * rotationSpeed.z);
+        Vector3 rotError = RotError(targetForce);
+        Vector3 rotErrorDelta = RotErrorDelta(rotError);
+        Vector3 rotationInput = RotationInput(rotError, rotErrorDelta);
         
-        Vector3 rotationDampeningInput = new Vector3(
-            DirErrorDelta.x * rotationDampening.x, 
-            DirErrorDelta.y * rotationDampening.y, 
-            DirErrorDelta.z * rotationDampening.z);
+        // Adjust propeller throttle to rotate drone towards target rotation
+        props[0].Throttle += rotationInput.x - rotationInput.z + rotationInput.y;
+        props[1].Throttle += -rotationInput.x - rotationInput.z - rotationInput.y;
+        props[2].Throttle += rotationInput.x + rotationInput.z - rotationInput.y;
+        props[3].Throttle += -rotationInput.x + rotationInput.z + rotationInput.y;
         
-        Vector3 directionInput = rotationProportionalInput + rotationDampeningInput;
-
-
-        Debug.Log(DirError.y);
-        props[0].Throttle += directionInput.x - directionInput.z + directionInput.y;
-        props[1].Throttle += -directionInput.x - directionInput.z - directionInput.y;
-        props[2].Throttle += directionInput.x + directionInput.z - directionInput.y;
-        props[3].Throttle += -directionInput.x + directionInput.z + directionInput.y;
-        
+        // Update previous physics values for calculating deltas
         _lastPosError = PosError;
-        _lastDirError = DirError;
+        _lastRotError = rotError;
+        _lastTargetPos = TargetPos;
     }
 }
--- Assets/Propeller.cs
+++ Assets/Propeller.cs
@@ -3,40 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Applies force and torque to a drone proportional to the <see cref="Throttle"/>
+/// </summary>
 public class Propeller : MonoBehaviour
 {
+    /// <summary>
+    /// The maximum amount of force the propeller can generate
+    /// </summary>
     public float MaxForce
     {
         get { return maxForce; }
         set { maxForce = value; }
     }
 
-    [SerializeField]
-    private float maxForce;
+    [SerializeField] private float maxForce;
 
     [SerializeField] private bool clockwise;
     
+    /// <summary>
+    /// The amount of force the propeller will apply. Limited by <see cref="MaxForce"/>
+    /// </summary>
     public float Throttle
     {
-        get { return throttle; }
-        set { throttle = value; }
+        get { return _throttle; }
+        set { _throttle = value; }
     }
     
-    [SerializeField] private float throttle;
+    private float _throttle;
 
+    /// <summary>
+    /// Propeller torque coefficient
+    /// </summary>
     [SerializeField] private float torque;
     
     [SerializeField] private Rigidbody rigid;
 
     private void FixedUpdate()
     {
+        // Apply Force
         Vector3 force = transform.up * Throttle;
         force = Vector3.ClampMagnitude(force, MaxForce);
         rigid.AddForceAtPosition(force, transform.position);
+        
+        // Apply Torque
         int direction;
         if (clockwise) direction = 1;
         else direction = -1;
         rigid.AddRelativeTorque(Vector3.up * (transform.localPosition.magnitude * force.magnitude * torque * direction));
+        
         Debug.DrawRay(transform.position, force, Color.green);
     }
 }
